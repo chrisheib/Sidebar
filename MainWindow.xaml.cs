598d8708945ad3a3622c145171fb26203c03cc47
STSchiff <chrisheib@gmx.de>
2019-12-24 14:32:49 +0100

    Sidebar erstellen, Close-Button

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfAppBar;
 
 namespace Sidebar
 {
@@ -23,6 +24,21 @@ namespace Sidebar
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            AppBarFunctions.SetAppBar(this, ABEdge.Right);
+        }
+
+        private void Window_Closing(object sender, EventArgs e)
+        {
+            AppBarFunctions.SetAppBar(this, ABEdge.None);
         }
     }
 }
