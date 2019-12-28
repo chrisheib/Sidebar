367cbed66a85902c50606ef8fa0159191cccf699
STSchiff <chrisheib@gmx.de>
2019-12-28 23:48:41 +0100

    Start auf 2tem Bildschirm, Layout

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 using WpfAppBar;
 
@@ -59,23 +50,24 @@ namespace Sidebar
                 DriveLeft.Children.Add(new Label()
                 {
                     Content = d.Name,
-                    Padding = new Thickness(0, 0, 0, 0),
-                    Height = 20,
+                    Padding = new Thickness(0, 2, 0, 0),
+                    FontSize = 10,
+                    Height = 15,
                     Foreground = Brushes.LightGray
                 });
                 var lbl = new Label()
                 {
-                    Content = d.Name,
-                    Padding = new Thickness(0, 0, 0, 0),
+                    Padding = new Thickness(0, 2, 4, 0),
+                    HorizontalAlignment = HorizontalAlignment.Right,
                     FontSize = 10,
                     Height = 15,
                     Foreground = Brushes.LightGray
                 };
-                DriveLeft.Children.Add(lbl);
+                DriveMiddle.Children.Add(lbl);
                 var bar = new ProgressBar()
                 {
                     Height = 10,
-                    Margin = new Thickness(0, 22, 0, 3),
+                    Margin = new Thickness(0, 4, 0, 1),
                     Background = Brushes.Gray
                 };
                 DriveRight.Children.Add(bar);
@@ -88,11 +80,13 @@ namespace Sidebar
         {
             new BackgroundWorkerProgressBar().Init(null, "Netzwerkschnittstelle", "Empfangene Bytes/s" , "Intel[R] Ethernet Connection [2] I219-V", 1000, null, (int value) =>
             {
-                NetworkDown.Value = ((value / 1024f) / ((110f * 1024f) / 8)) * 100f;
+                NetworkDown.Value = Math.Truncate(value / 1024f / (115f * 1024f / 8f) * 100f);
+                NetworkDownNumber.Content = GetBytesReadable(value);
             });
             new BackgroundWorkerProgressBar().Init(null, "Netzwerkschnittstelle", "Bytes gesendet/s", "Intel[R] Ethernet Connection [2] I219-V", 1000, null,(int value) => 
             {
-                NetworkUp.Value = ((value / 1024f) / ((55f*1024f)/8f)) * 100f;
+                NetworkUp.Value = Math.Truncate(value / 1024f / (60f * 1024f / 8f) * 100f);
+                NetworkUpNumber.Content = GetBytesReadable(value);
             });
         }
 
@@ -103,12 +97,77 @@ namespace Sidebar
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            AppBarFunctions.SetAppBar(this, ABEdge.Right);
+            // Auf 2tem Bildschirm öffnen
+            if (System.Windows.Forms.Screen.AllScreens.Length > 1)
+            {
+                System.Windows.Forms.Screen s2 = System.Windows.Forms.Screen.AllScreens[1];
+                System.Drawing.Rectangle r2 = s2.WorkingArea;
+                Top = r2.Top;
+                Left = r2.Left;
+            }
+            else
+            {
+                System.Windows.Forms.Screen s1 = System.Windows.Forms.Screen.AllScreens[0];
+                System.Drawing.Rectangle r1 = s1.WorkingArea;
+                Top = r1.Top;
+                Left = r1.Left;
+            }
+
+            AppBarFunctions.SetAppBar(this, ABEdge.Left);
         }
 
         private void Window_Closing(object sender, EventArgs e)
         {
             AppBarFunctions.SetAppBar(this, ABEdge.None);
+        }
+        
+        // Returns the human-readable file size for an arbitrary, 64-bit file size 
+        // The default format is "0.### XB", e.g. "4.2 KB" or "1.434 GB"
+        public string GetBytesReadable(long i)
+        {
+            // Get absolute value
+            long absolute_i = (i < 0 ? -i : i);
+            // Determine the suffix and readable value
+            string suffix;
+            double readable;
+            if (absolute_i >= 0x1000000000000000) // Exabyte
+            {
+                suffix = "EB/s";
+                readable = (i >> 50);
+            }
+            else if (absolute_i >= 0x4000000000000) // Petabyte
+            {
+                suffix = "PB/s";
+                readable = (i >> 40);
+            }
+            else if (absolute_i >= 0x10000000000) // Terabyte
+            {
+                suffix = "TB/s";
+                readable = (i >> 30);
+            }
+            else if (absolute_i >= 0x40000000) // Gigabyte
+            {
+                suffix = "GB/s";
+                readable = (i >> 20);
+            }
+            else if (absolute_i >= 0x100000) // Megabyte
+            {
+                suffix = "MB/s";
+                readable = (i >> 10);
+            }
+            else if (absolute_i >= 0x400) // Kilobyte
+            {
+                suffix = "KB/s";
+                readable = i;
+            }
+            else
+            {
+                return i.ToString("0 B/s"); // Byte
+            }
+            // Divide by 1024 to get fractional value
+            readable = (readable / 1024);
+            // Return formatted number with suffix
+            return readable.ToString("0.### ") + suffix;
         }
     }
 }
