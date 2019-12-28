5c07523c1d0a20683a3ff2ef3b9436810e1d323a
STSchiff <chrisheib@gmx.de>
2019-12-28 15:20:27 +0100

    BackgroundWorkerProgressBar-Class, Label, All Cores,

--- BackgroundWorkerProgressBar.cs
+++ BackgroundWorkerProgressBar.cs
@@ -10,34 +10,35 @@ namespace Sidebar
     /// <summary>    
     /// It implements backgroundworker.   
     /// </summary>    
-    class CPU
+    class BackgroundWorkerProgressBar
     {
 
-        private static BackgroundWorker backgroundWorker;
-        private static ProgressBar bar;
-        private static PerformanceCounter perfCounter;
+        private BackgroundWorker backgroundWorker;
+        private ProgressBar bar;
+        private Label label;
+        private PerformanceCounter perfCounter;
+        private int repeat;
 
-        public static void Init(ProgressBar oBar)
+        public void Init(ProgressBar bar, string category, string value, string param, int repeat = 1000, Label label = null)
         {
 
-            bar = oBar;
-            perfCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            this.repeat = repeat;
+            this.label = label;
+            this.bar = bar;
+
+            perfCounter = new PerformanceCounter(category, value, param);
 
             backgroundWorker = new BackgroundWorker
             {
-
                 WorkerReportsProgress = true,
                 WorkerSupportsCancellation = true
-
             };
 
             //Event creation.    
             //For the performing operation in the background.    
             backgroundWorker.DoWork += BackgroundWorker_DoWork; 
             backgroundWorker.ProgressChanged += BackgroundWorker_ProgressChanged;
-            backgroundWorker.RunWorkerCompleted += BackgroundWorker_RunWorkerCompleted;
             backgroundWorker.RunWorkerAsync();
-
         }
 
         /// <summary>    
@@ -45,27 +46,18 @@ namespace Sidebar
         /// </summary>    
         /// <param name="sender"></param>    
         /// <param name="e"></param>    
-
-        static void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
         {
-
             while (true)
             {
-
                 if (backgroundWorker.CancellationPending)
                 {
-
                     e.Cancel = true;
                     return;
-
                 }
-
                 backgroundWorker.ReportProgress((int)Math.Truncate((double) perfCounter.NextValue()));
-                Thread.Sleep(1000);
-                e.Result = 1000;
-
+                Thread.Sleep(repeat);
             }
-
         }
 
         /// <summary>    
@@ -73,44 +65,13 @@ namespace Sidebar
         /// </summary>    
         /// <param name="sender"></param>    
         /// <param name="e"></param>    
-
-        static void BackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        void BackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-
             bar.Value = e.ProgressPercentage;
-
-        }
-
-        /// <summary>    
-        /// Displays result of background performing operation.    
-        /// </summary>    
-        /// <param name="sender"></param>    
-        /// <param name="e"></param>    
-
-        static void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
-        {
-
-            if (e.Cancelled)
+            if (label != null)
             {
-
-                Console.WriteLine("Operation Cancelled");
-
+                label.Content = e.ProgressPercentage.ToString() + "%";
             }
-
-            else if (e.Error != null)
-            {
-
-                Console.WriteLine("Error in Process :" + e.Error);
-
-            }
-
-            else
-            {
-
-                Console.WriteLine("Operation Completed :" + e.Result);
-
-            }
-
         }
 
     }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,6 +24,15 @@ namespace Sidebar
         public MainWindow()
         {
             InitializeComponent();
+            new BackgroundWorkerProgressBar().Init(pbCPUTotal, "Processor", "% Processor Time", "_Total", 1000, lblCPUTotal);
+            new BackgroundWorkerProgressBar().Init(pbCPUCore1, "Processor", "% Processor Time", "0", 1000, lblCPUCore1);
+            new BackgroundWorkerProgressBar().Init(pbCPUCore2, "Processor", "% Processor Time", "1", 1000, lblCPUCore2);
+            new BackgroundWorkerProgressBar().Init(pbCPUCore3, "Processor", "% Processor Time", "2", 1000, lblCPUCore3);
+            new BackgroundWorkerProgressBar().Init(pbCPUCore4, "Processor", "% Processor Time", "3", 1000, lblCPUCore4);
+            new BackgroundWorkerProgressBar().Init(pbCPUCore5, "Processor", "% Processor Time", "4", 1000, lblCPUCore5);
+            new BackgroundWorkerProgressBar().Init(pbCPUCore6, "Processor", "% Processor Time", "5", 1000, lblCPUCore6);
+            new BackgroundWorkerProgressBar().Init(pbCPUCore7, "Processor", "% Processor Time", "6", 1000, lblCPUCore7);
+            new BackgroundWorkerProgressBar().Init(pbCPUCore8, "Processor", "% Processor Time", "7", 1000, lblCPUCore8);
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
@@ -34,7 +43,6 @@ namespace Sidebar
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             AppBarFunctions.SetAppBar(this, ABEdge.Right);
-            CPU.Init(pbCPUTotal);
         }
 
         private void Window_Closing(object sender, EventArgs e)
